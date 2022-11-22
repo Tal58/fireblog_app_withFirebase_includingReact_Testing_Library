d5f1600489f550d3c13ff70a76e51215f720f882
Mihail Tal <mihail.tal85@gmail.com>
2022-11-22 15:53:46 +0300

    persist added

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,5 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 //make store component for authReducer
 
@@ -7,6 +17,12 @@ const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
   devTools: process.env.NODE_ENV !== "production",
 });
 export default store;
